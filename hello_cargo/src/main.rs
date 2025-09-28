bfc4998e59344f5a4e9d397080e3b924891ec131
Nikhil Sharma <nikhilijs@gmail.com>
2025-09-29 00:12:56 +0530

    mutable immutable

--- hello_cargo/src/main.rs
+++ hello_cargo/src/main.rs
@@ -2,5 +2,12 @@ fn main() {
     println!("Hello, world!");
 
     let num:u8 = 5;
-    println!("the value of num is {}", num)
+    println!("the value of num is {}", num);
+    // variables are by default immutable 
+    //but if we want to change them to mutable 
+    //then 
+
+    let mut nik:u16 = 23;
+    nik = nik +24;
+    println!("the value of nik is {}",nik);
 }
