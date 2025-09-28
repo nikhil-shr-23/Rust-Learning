1ce9a52a47cc07025ec774ed68691ec84481efd3
Nikhil Sharma <nikhilijs@gmail.com>
2025-09-29 01:25:32 +0530

    rust

---
+++ hello_cargo/src/new.rs
@@ -0,0 +1,17 @@
+fn main(){
+    println!("this is a new file");
+
+    //learning strings
+
+   // let nik:&str = "this is a string, yes";
+
+   // println!("this is a string:{},",nik)
+
+
+   let str1:String= String::from("hello nikhil");
+   let str2:String= String::from("hello nikhil");
+   println!("str1:{}",str1);
+   println!("str2:{}",str2);
+
+
+}
