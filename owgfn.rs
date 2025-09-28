efe5017d241f6d44fa778767581a7e76e3f6a02f
Nikhil Sharma <nikhilijs@gmail.com>
2025-09-29 03:24:15 +0530

    ownership functions

---
+++ owgfn.rs
@@ -0,0 +1,16 @@
+fn main(){
+    let x:String = String::from("Maalik");
+    yaar(x);
+    println!("the value of x is {}",x);
+    //shouldn't work
+}
+
+
+fn yaar(item:String){
+    println!("the value of item is {}",item);
+}
+
+
+//the error is 
+// println!("the value of x is {}",x);
+                              //    ^ value borrowed here after move
